3613d3bda6880d8cfe8557e964a5cb91e94dea4b
Román Cárdenas <rcardenas.rod@gmail.com>
2023-11-02 11:19:05 +0100

    ready for RT

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 #![no_std]
-#![feature(trace_macros)]
-
-trace_macros!(true);
 
 pub use xdevs_no_std_macros::*;
 
--- src/simulator.rs
+++ src/simulator.rs
@@ -1,22 +1,54 @@
 use crate::aux::AbstractSimulator;
 
-#[repr(transparent)]
-pub struct Simulator<T>(T);
+#[cfg(feature = "std")]
+pub mod std;
 
-impl<T: AbstractSimulator> Simulator<T> {
+/// A DEVS simulator that uses a virtual clock (i.e., no real time is used).
+pub struct Simulator<M: AbstractSimulator> {
+    model: M,
+}
+
+impl<M: AbstractSimulator> Simulator<M> {
     #[inline]
-    pub const fn new(simulator: T) -> Self {
-        Self(simulator)
+    pub const fn new(model: M) -> Self {
+        Self { model }
     }
 
     /// It executes the simulation of the inner DEVS model from `t_start` to `t_stop`.
+    /// It provides support for real time execution via the following arguments:
+    ///
+    /// - `wait_until`: a closure that is called between state transitions.
+    /// It receives the time of the next state transition and a mutable reference to the input ports.
+    /// It returns the actual time "waited".
+    /// If the returned time is equal to the input time, an internal/confluent state transition is performed.
+    /// Otherwise, it assumes that an external event happened and executes the external transition function.
+    /// - `propagate_output`: a closure that is called after output functions.
+    /// It receives a mutable reference to the output ports so the closure can access to output events.
+    /// Feel free to ignore this argument if you do not need to propagate output messages.
     #[inline]
-    pub fn simulate(&mut self, t_start: f64, t_stop: f64) {
-        let mut t_next = self.0.start(t_start);
+    pub fn simulate_rt(
+        &mut self,
+        t_start: f64,
+        t_stop: f64,
+        mut wait_until: impl FnMut(f64, &mut M::Input) -> f64,
+        mut propagate_output: impl FnMut(&M::Output),
+    ) {
+        let mut t_next = self.model.start(t_start);
         while t_next < t_stop {
-            self.0.lambda(t_next);
-            t_next = self.0.delta(t_next);
+            let t = wait_until(t_next, self.model.get_input_mut());
+            if t >= t_next {
+                self.model.lambda(t);
+                propagate_output(self.model.get_output());
+            }
+            t_next = self.model.delta(t);
         }
-        self.0.stop(t_stop);
+        self.model.stop(t_stop);
+    }
+
+    /// It executes the simulation of the inner DEVS model from `t_start` to `t_stop`.
+    /// It uses a virtual clock (i.e., no real time is used).
+    #[inline]
+    pub fn simulate_vt(&mut self, t_start: f64, t_stop: f64) {
+        self.simulate_rt(t_start, t_stop, |t, _| t, |_| {});
     }
 }
