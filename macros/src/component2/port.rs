14056f00c1b3a2f33b6031cf70c71c1e3acbc05f
dmunizu <d.muniz@alumnos.upm.es>
2025-11-17 09:11:04 +0100

    Functional Iterable types declaration.

--- macros/src/component2/port.rs
+++ macros/src/component2/port.rs
@@ -1,6 +1,7 @@
 use proc_macro2::Ident;
 use proc_macro2::TokenStream as TokenStream2;
 use syn::Generics;
+use syn::Type;
 use syn::TypeGenerics;
 
 use super::Field;
@@ -20,10 +21,24 @@ impl Ports {
         ty_generics
     }
 
+    fn generate_news(&self, ports: &Vec<Field>) -> Vec<TokenStream2> {
+        let mut news:Vec<TokenStream2> = Vec::new();
+        for port in ports {
+            let port_ident = &port.ident;
+            let token = extract_new(&port.ty);
+            let new = quote::quote!{
+                #port_ident: #token
+            };
+            news.push(new);
+        }
+        news
+    }
+
     pub fn quote(&self, ident: &Ident) -> TokenStream2 {
         let ports_ident: Vec<_> = self.ports.iter().map(|f| &f.ident).collect();
         let ports_ty: Vec<_> = self.ports.iter().map(|f| &f.ty).collect();
         let (impl_generics, ty_generics, _) = self.generics.split_for_impl();
+        let new_fn = self.generate_news(&self.ports);
 
         quote::quote! {
             #[derive(Debug, Default)]
@@ -33,7 +48,7 @@ impl Ports {
             impl #impl_generics #ident #ty_generics {
                 #[inline]
                 pub const fn new() -> Self {
-                    Self { #(#ports_ident: xdevs::port::Port::new()),* }
+                    Self { #(#new_fn),* }
                 }
             }
             unsafe impl #impl_generics xdevs::traits::Bag for #ident #ty_generics{
@@ -48,4 +63,37 @@ impl Ports {
             }
         }
     }
+}
+
+fn extract_new(ty: &Type) -> TokenStream2 {
+    let token: TokenStream2 = match ty {
+        Type::Array(array) => {
+            let token = extract_new(&*array.elem);
+            let length = &array.len;
+            
+            // Try to parse length as a literal integer
+            if let syn::Expr::Lit(syn::ExprLit { lit: syn::Lit::Int(lit_int), .. }) = length {
+                let n: usize = lit_int.base10_parse().unwrap();
+                let repeated: Vec<_> = (0..n).map(|_| quote::quote! { #token }).collect();
+                quote::quote! {
+                    [ #( #repeated ),* ]
+                }
+            } else {
+                quote::quote! {
+                    compile_error!("Array length must be a literal integer for non-Copy types");
+                }
+            }
+        }
+        Type::Path(path) => {
+            let mut path = path.path.clone();
+            for segment in &mut path.segments {
+                segment.arguments = syn::PathArguments::None;
+            }
+            quote::quote!{
+                #path::new()
+            }
+        }
+        &_ => unimplemented!()
+    };
+    token
 }
--- src/impls.rs
+++ src/impls.rs
@@ -6,6 +6,82 @@ use crate::traits::AbstractSimulator;
 use crate::traits::Bag;
 use crate::traits::Component;
 
+//////////////////////////////////////////////// Iterables //////////////////////////////////////////////
+
+macro_rules! seq_impl_body {
+    () => {
+        fn is_empty(&self) -> bool {
+            self.iter().all(|bag| bag.is_empty())
+        }
+
+        fn clear(&mut self) {
+            for bag in self.iter_mut() {
+                bag.clear();
+            }
+        }
+    };
+}
+
+#[cfg(any(feature = "std", feature = "alloc"))]
+unsafe impl<T: Bag> Bag for alloc::vec::Vec<T> {
+    seq_impl_body!();
+}
+
+unsafe impl<T: Bag, const N: usize> Bag for heapless::Vec<T, N> {
+    seq_impl_body!();
+}
+unsafe impl<T: Bag, const N: usize> Bag for [T; N] {
+    seq_impl_body!();
+}
+
+//////////////////////////////////////////////// Tuples //////////////////////////////////////////////
+
+unsafe impl Bag for () {
+    fn is_empty(&self) -> bool {
+        true
+    }
+
+    fn clear(&mut self) {}
+}
+
+// Macro to implement Bag for tuples
+macro_rules! impl_bag_for_tuples {
+    ( $( $name:ident ),+ ) => {
+        #[allow(non_snake_case)]
+        unsafe impl<$( $name ),+> Bag for ( $( $name ),+ )
+        where
+            $( $name: Bag ),+
+        {
+            fn is_empty(&self) -> bool {
+                let ( $( $name ),+ ) = self;
+                true $( && $name.is_empty() )+
+            }
+
+            fn clear(&mut self) {
+                let ( $( $name ),+ ) = self;
+                $( $name.clear(); )+
+            }
+        }
+    };
+}
+
+// Generate impls for tuples of size 2 up to, say, 12
+impl_bag_for_tuples!(T1, T2);
+impl_bag_for_tuples!(T1, T2, T3);
+impl_bag_for_tuples!(T1, T2, T3, T4);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15);
+impl_bag_for_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16);
+
 //////////////////////////////////////////////// References //////////////////////////////////////////////
 
 macro_rules! impl_ref {
