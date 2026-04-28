cbe22d2d25510db796355d9640c8cde30229b998
dmunizu <d.muniz@alumnos.upm.es>
2026-04-28 12:13:37 +0200

    Remove legacy component and coupled macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,40 +1,12 @@
 use proc_macro::TokenStream;
 
-mod component;
 mod component2;
 mod derive;
-
-#[proc_macro]
-pub fn component(input: TokenStream) -> TokenStream {
-    let component: component::Component = syn::parse_macro_input!(input);
-    component.quote().into()
-}
-
-// #[proc_macro]
-// pub fn atomic(input: TokenStream) -> TokenStream {
-//     let atomic_meta: atomic::AtomicMeta = syn::parse_macro_input!(input);
-//     atomic_meta.quote().into()
-// }
-
-// #[proc_macro]
-// pub fn coupled(input: TokenStream) -> TokenStream {
-//     let coupled_meta: coupled::CoupledMeta = syn::parse_macro_input!(input);
-//     coupled_meta.quote().into()
-// }
 
 #[proc_macro_attribute]
 pub fn atomic(args: TokenStream, item: TokenStream) -> TokenStream {
     let atomic_component = component2::atomic::Component::parse(args.into(), item.into());
     match atomic_component {
-        Ok(component) => component.quote().into(),
-        Err(err) => err.to_compile_error().into(),
-    }
-}
-
-#[proc_macro_attribute]
-pub fn coupled(args: TokenStream, item: TokenStream) -> TokenStream {
-    let coupled_component = component2::coupled::Component::parse(args.into(), item.into());
-    match coupled_component {
         Ok(component) => component.quote().into(),
         Err(err) => err.to_compile_error().into(),
     }
