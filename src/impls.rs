ffb9e6a504986cfb15413d3a2989872b0c254bc5
dmunizu <d.muniz@alumnos.upm.es>
2026-05-18 15:29:14 +0200

    Remove unnecessary trait methods and types
    Remove methods and types that will no longer be necessary with the new system where the outer coupled model will hold the input and output of their inner components.

--- src/impls.rs
+++ src/impls.rs
@@ -28,14 +28,6 @@ impl<T: AsPort, const N: usize> Sealed for [T; N] {}
 unsafe impl<T: Component, const N: usize> Component for [T; N] {
     type Input = ();
     type Output = ();
-    type InputRef<'a>
-        = [T::InputRef<'a>; N]
-    where
-        Self: 'a;
-    type OutputRef<'a>
-        = [T::OutputRef<'a>; N]
-    where
-        Self: 'a;
 
     fn get_t_last(&self) -> f64 {
         self.iter()
@@ -55,65 +47,6 @@ unsafe impl<T: Component, const N: usize> Component for [T; N] {
 
     fn set_t_next(&mut self, t_next: f64) {
         self.iter_mut().for_each(|c| c.set_t_next(t_next));
-    }
-
-    fn get_input(&self) -> &Self::Input {
-        unimplemented!("get_input is not supported for collections; access elements directly")
-    }
-
-    fn get_input_mut(&mut self) -> &mut Self::Input {
-        unimplemented!("get_input_mut is not supported for collections; access elements directly")
-    }
-
-    fn get_output(&self) -> &Self::Output {
-        unimplemented!("get_output is not supported for collections; access elements directly")
-    }
-
-    fn get_output_mut(&mut self) -> &mut Self::Output {
-        unimplemented!("get_output_mut is not supported for collections; access elements directly")
-    }
-
-    fn get_ports(&mut self) -> (Self::InputRef<'_>, Self::OutputRef<'_>) {
-        // SAFETY: An uninitialized `[MaybeUninit<_>; N]` is valid.
-        let mut inputs: [core::mem::MaybeUninit<T::InputRef<'_>>; N] =
-            unsafe { core::mem::MaybeUninit::uninit().assume_init() };
-        let mut outputs: [core::mem::MaybeUninit<T::OutputRef<'_>>; N] =
-            unsafe { core::mem::MaybeUninit::uninit().assume_init() };
-
-        for (i, component) in self.iter_mut().enumerate() {
-            let (input, output) = component.get_ports();
-            inputs[i].write(input);
-            outputs[i].write(output);
-        }
-
-        // SAFETY: All elements have been initialized in the loop above.
-        unsafe {
-            (
-                inputs.map(|m| m.assume_init()),
-                outputs.map(|m| m.assume_init()),
-            )
-        }
-    }
-
-    fn get_out_ports(&self) -> Self::OutputRef<'_> {
-        // SAFETY: An uninitialized `[MaybeUninit<_>; N]` is valid.
-        let mut outputs: [core::mem::MaybeUninit<T::OutputRef<'_>>; N] =
-            unsafe { core::mem::MaybeUninit::uninit().assume_init() };
-
-        for (i, component) in self.iter().enumerate() {
-            outputs[i].write(component.get_out_ports());
-        }
-
-        // SAFETY: All elements have been initialized in the loop above.
-        unsafe { outputs.map(|m| m.assume_init()) }
-    }
-
-    fn clear_input(&mut self) {
-        self.iter_mut().for_each(|c| c.clear_input());
-    }
-
-    fn clear_output(&mut self) {
-        self.iter_mut().for_each(|c| c.clear_output());
     }
 }
 
@@ -159,14 +92,6 @@ macro_rules! impl_ref {
         unsafe impl<T: Component> Component for $ty {
             type Input = T::Input;
             type Output = T::Output;
-            type InputRef<'a>
-                = T::InputRef<'a>
-            where
-                Self: 'a;
-            type OutputRef<'a>
-                = T::OutputRef<'a>
-            where
-                Self: 'a;
 
             fn get_t_last(&self) -> f64 {
                 (**self).get_t_last()
@@ -182,30 +107,6 @@ macro_rules! impl_ref {
 
             fn set_t_next(&mut self, t_next: f64) {
                 (**self).set_t_next(t_next)
-            }
-
-            fn get_input(&self) -> &Self::Input {
-                (**self).get_input()
-            }
-
-            fn get_input_mut(&mut self) -> &mut Self::Input {
-                (**self).get_input_mut()
-            }
-
-            fn get_output(&self) -> &Self::Output {
-                (**self).get_output()
-            }
-
-            fn get_output_mut(&mut self) -> &mut Self::Output {
-                (**self).get_output_mut()
-            }
-
-            fn get_ports(&mut self) -> (Self::InputRef<'_>, Self::OutputRef<'_>) {
-                (**self).get_ports()
-            }
-
-            fn get_out_ports(&self) -> Self::OutputRef<'_> {
-                (**self).get_out_ports()
             }
         }
 
--- src/lib.rs
+++ src/lib.rs
@@ -52,15 +52,15 @@ pub trait Coupled: traits::PartialCoupled {
     /// External Input Coupling. Propagates input events from the coupled model to its inner components.
     #[allow(unused_variables)]
     #[inline]
-    fn eic(from: &Self::Input, to: &mut Self::ComponentsInput<'_>) {}
+    fn eic(from: &Self::Input, to: &mut Self::ComponentsInput) {}
 
     /// Internal Coupling. Propagates output events from inner components to input events of other inner components.
     #[allow(unused_variables)]
     #[inline]
-    fn ic(from: &Self::ComponentsOutput<'_>, to: &mut Self::ComponentsInput<'_>) {}
+    fn ic(from: &Self::ComponentsOutput, to: &mut Self::ComponentsInput) {}
 
     /// External Output Coupling. Propagates output events from inner components to the coupled model's output.
     #[allow(unused_variables)]
     #[inline]
-    fn eoc(from: &Self::ComponentsOutput<'_>, to: &mut Self::Output) {}
+    fn eoc(from: &Self::ComponentsOutput, to: &mut Self::Output) {}
 }
