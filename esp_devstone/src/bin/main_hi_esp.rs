48372647a8008e7cf043fb630c45b7e316d3f3b1
cristinarodriguezdiaz <cristina.rdiaz@alumnos.upm.es>
2026-05-25 18:42:11 +0200

    corrección ortografía width

--- esp_devstone/src/bin/main_hi_esp.rs
+++ esp_devstone/src/bin/main_hi_esp.rs
@@ -35,11 +35,11 @@ fn main() -> ! {
 
     esp_alloc::heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 65536);
 
-    const WIDHT: usize = 2;
-    const W: usize = WIDHT - 1;
+    const WIDTH: usize = 33;
+    const W: usize = WIDTH - 1;
 
     let start = Instant::now();
-    xdevs_devstone_macros::generate_hi!(2, 2);
+    xdevs_devstone_macros::generate_hi!(33, 33);
     let generator = Generator::new(5);
     let modelo_final: ModeloFinal<W> = ModeloFinal::build(generator, model_hi);
     let duration: Duration = start.elapsed();
